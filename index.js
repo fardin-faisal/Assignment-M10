17b989e00d7eff31377a2de4ae40f21a319ac82b
Mohammed Faisal Fardin Chowdhury <fardin00faisal05@gmail.com>
2024-09-19 19:57:07 +0600

    Finding Smallest Number function added!

--- index.js
+++ index.js
@@ -19,3 +19,13 @@ function testIsOdd() {
   const num = parseInt(document.getElementById('oddNum').value);
   document.getElementById('oddResult').innerText = isOdd(num) ? "The number is odd" : "The number is not odd";
 }
+
+// 3. finds Min Number
+function findMin(arr) {
+    return Math.min(...arr); //'...' is the spread operator unpacks the array into separate values for Math.min() to process.
+}
+
+function testFindMin() {
+    const arr = document.getElementById('minArray').value.split(',').map(Number);
+    document.getElementById('minResult').innerText = "Minimum: " + findMin(arr);
+}
