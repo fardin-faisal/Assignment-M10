d753bb735a42f60771ea42a2c6f5023bafc59c66
Mohammed Faisal Fardin Chowdhury <fardin00faisal05@gmail.com>
2024-09-19 20:24:02 +0600

    lowercaseFirstLetter Function Added!

--- index.js
+++ index.js
@@ -16,28 +16,31 @@ function isOdd(num) {
 }
 
 function testIsOdd() {
-  const num = parseInt(document.getElementById('oddNum').value);
-  document.getElementById('oddResult').innerText = isOdd(num) ? "The number is odd" : "The number is not odd";
+  const num = parseInt(document.getElementById("oddNum").value);
+  document.getElementById("oddResult").innerText = isOdd(num)
+    ? "The number is odd"
+    : "The number is not odd";
 }
 
 // 3. finds Min Number
 function findMin(arr) {
-    return Math.min(...arr); //'...' is the spread operator unpacks the array into separate values for Math.min() to process.
+  return Math.min(...arr); //'...' is the spread operator unpacks the array into separate values for Math.min() to process.
 }
 
 function testFindMin() {
-    const arr = document.getElementById('minArray').value.split(',').map(Number);
-    document.getElementById('minResult').innerText = "Minimum: " + findMin(arr);
+  const arr = document.getElementById("minArray").value.split(",").map(Number);
+  document.getElementById("minResult").innerText = "Minimum: " + findMin(arr);
 }
 
 // 4. filtering Even Numbers
 function filterEvenNumbers(arr) {
-  return arr.filter(num => num % 2 === 0);
+  return arr.filter((num) => num % 2 === 0);
 }
 
 function testFilterEvenNumbers() {
-  const arr = document.getElementById('evenArray').value.split(',').map(Number);
-  document.getElementById('evenResult').innerText = "Even Numbers are: " + filterEvenNumbers(arr).join(', ');
+  const arr = document.getElementById("evenArray").value.split(",").map(Number);
+  document.getElementById("evenResult").innerText =
+    "Even Numbers are: " + filterEvenNumbers(arr).join(", ");
 }
 
 // 5. sort Array in Descending order
@@ -46,6 +49,18 @@ function sortArrayDescending(arr) {
 }
 
 function testSortArrayDescending() {
-  const arr = document.getElementById('sortArray').value.split(',').map(Number);
-  document.getElementById('sortResult').innerText = "Sorted Array In Descending Order: " + sortArrayDescending(arr).join(', ');
+  const arr = document.getElementById("sortArray").value.split(",").map(Number);
+  document.getElementById("sortResult").innerText =
+    "Sorted Array In Descending Order: " + sortArrayDescending(arr).join(", ");
+}
+
+// 6. lowercase First Letter fo a String
+function lowercaseFirstLetter(str) {
+  return str.charAt(0).toLowerCase() + str.slice(1);
+}
+
+function testLowercaseFirstLetter() {
+  const str = document.getElementById("lowerString").value;
+  document.getElementById("lowerResult").innerText =
+    "Modified String: " + lowercaseFirstLetter(str);
 }
